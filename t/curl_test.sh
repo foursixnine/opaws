6a45e78060c2f073976bdb112484bc3acb6bc987
Santiago Zarate <santiago.zarate@suse.com>
2017-02-24 16:05:09 +0100

    Add first try to generate build reports

--- t/curl_test.sh
+++ t/curl_test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 export SERVER="${1:-"http://localhost:3000"}"
-curl -i -k -H "Content-Type: application/json" --data @data/body.json $SERVER
+export INTENT="${2:-"cloneJob"}"
+export INTENTFILE="data/$INTENT.json"
+
+if [ ! -e $INTENTFILE ]; then
+    echo "$INTENT was not found, available intents are:"
+    for file in $(ls data/); do
+         echo $file | sed s/\.json// | xargs echo
+    done
+    exit 1
+fi
+
+curl -i -k -H "Content-Type: application/json" --data @$INTENTFILE $SERVER
